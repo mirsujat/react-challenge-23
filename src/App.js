b310dd579a2853776cfedf9c365ff8fcb2e22fe4
Mir Sujat <mir.sujat@gmail.com>
2023-01-24 21:52:15 +0600

    mmm

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Tabs from './Components/Tabs/Tabs';
 
 
 
@@ -7,7 +8,10 @@ const  App = () => {
   return (
     <div className="App">
       Hello world From App
-     
+      <Tabs>
+        <div label="tab one">Tab ....one content</div>
+        <div label="tab two">Tab two... content</div>
+      </Tabs>
     </div>
   );
 }
---
+++ src/Components/Tab/Tab.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tab = (props) => {
+    const onClick = () => props.onClick(props.label);
+
+    return (
+    <li onClick={onClick} >{props.label}</li>
+    );
+}
+
+export default Tab;
