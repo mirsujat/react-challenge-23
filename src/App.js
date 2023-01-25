a8a3c3f39e70cfec3cdc87a5635ac7f44b5d1fa9
Mir Sujat <mir.sujat@gmail.com>
2023-01-25 16:26:47 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import Tabs from './Components/Tabs/Tabs';
+import Accordion from './Components/Accordion/Accordion';
+
 
 
 
@@ -9,12 +10,7 @@ const  App = () => {
   return (
     <div className="App">
       Hello world From App
-   
-      <Tabs>
-        <div label="tab one">Tab one... content</div>
-        <div label="tab two">Tab ...two content</div>
-      </Tabs>
-      
+         
     </div>
   );
 }
