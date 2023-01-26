dad0bc5b9e2ecf5b346493bbe917819ec0f16d5c
Mir Sujat <mir.sujat@gmail.com>
2023-01-26 22:35:26 +0600

    Update Accordion.js

--- src/Components/Accordion/Accordion.js
+++ src/Components/Accordion/Accordion.js
@@ -4,9 +4,12 @@ class Accordion extends Component {
     constructor(props) {
         super(props);
             
-        this.state = {}
+        this.state = {
+           
+        }
     }
 
+  
 
     render() {
         
@@ -16,8 +19,16 @@ class Accordion extends Component {
                     {this.props.children.map((child, i) =>{
                         return(
                             <div key={i} >
-                                <p>{child.props.label}</p>
-                                {child}
+                                <p
+                                
+                                >
+                                {child.props.label}
+                                </p>
+                                {
+                                this.state.activeTab === child.props.label || this.state.isOpen 
+                                ? child : null
+        
+                                }
                             </div>
                         )
                     })}
