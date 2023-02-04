85f5b647b92ce3cb77a65d2b1a0ef79a97913d28
Mir Sujat <mir.sujat@gmail.com>
2023-02-04 12:46:00 +0600

    mir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Accordion from './Components/Accordion/Accordion';
+
 
 
 
@@ -10,20 +10,7 @@ const  App = () => {
   return (
     <div className="App">
       Hello world From App
-         <Accordion >
-            <div label="one" >
-              <div>Accordion one</div>
-              <div>Accordion one</div>
-            </div>
-            <div label="two">
-              <div>Accordion two</div>
-              <div>Accordion two</div>
-            </div>
-            <div label="three">
-              <div>Accordion three</div>
-              <div>Accordion three</div>
-            </div>
-         </Accordion>
+         
     </div>
   );
 }
