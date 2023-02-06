672d285ad773d65b3bb23a85483495b4ef9033da
Mir Sujat <mir.sujat@gmail.com>
2023-02-06 22:37:51 +0600

    Update SearchInput.js

--- src/Components/Search/SearchInput.js
+++ src/Components/Search/SearchInput.js
@@ -5,39 +5,41 @@ class SearchInput extends Component {
         super(props);
         
         this.state = { 
-            result: "",
-            searchTerm: "me", 
+            result: null,
+            searchTerm: "me",
+            loading: true, 
             };
     }
 
 componentDidMount(){
     const {searchTerm} = this.state;
-    const regex = new RegExp("\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b");
-    if(searchTerm === "me" || regex.test(searchTerm)){
             fetch(`https://ip.nf/${searchTerm}.json`)
                 .then(res => res.json())
                 .then(res => Object.assign({}, res.ip))
-                .then(res => this.setState({result: res})); 
-    }else{
-        return;
-    }
-    
+                .then(res => this.setState({result: res, loading: false})); 
 };
 
-shouldComponentUpdate(nextProps, nextState){
+componentDidUpdate(prevProps, prevState){
     const {searchTerm} = this.state;
-    const regex = new RegExp(`/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/`);       
-       if(searchTerm !== nextState.searchTerm && regex.test(searchTerm)){
+    if(this.state.searchTerm !== prevState.searchTerm){
          fetch(`https://ip.nf/${searchTerm}.json`)
                 .then(res => res.json())
                 .then(res => Object.assign({}, res.ip))
-                .then(res => this.setState({result: res})); 
-       }else{
-            return false;
-       }
-      
- 
+                .then(res => this.setState({result: res, loading: false})); 
+    }
 }
+
+// shouldComponentUpdate(nextProps, nextState){
+//     const {searchTerm} = this.state;
+//     //const regex = new RegExp(`/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/`);       
+//        if(searchTerm !== nextState.searchTerm ){
+//           return true;
+//        }
+//        if(this.props.value !== nextProps.value){
+//         return true;
+//        }
+//        return false;
+// }
 
 
     handleChange = (e) =>{
@@ -45,12 +47,25 @@ shouldComponentUpdate(nextProps, nextState){
         this.setState({ searchTerm : value });
     }
 
+    
+
     render() {
-        console.log(" Return result", this.state.result);
-        console.log("Return IP length:", this.state.result.toString(this.state.result.ip).length);
-        console.log("searchTerm:", this.state.searchTerm);
-        
+        // console.log(" Return result", this.state.result);
+        // console.log("Return IP length:", this.state.result.toString(this.state.result.ip).length);
+        // console.log("searchTerm:", this.state.searchTerm);
+        let content;
+        if(this.state.loading === true){
+             content = <div>Loading....</div>
+
+        }
+        if(this.state.loading === false){
+           content = <div>{this.state.result.ip}</div>
+            
+        }
+       
+
         return (
+            <div>
             <form>
                 <input 
                 type="text" 
@@ -63,8 +78,10 @@ shouldComponentUpdate(nextProps, nextState){
                 <br></br>
                 <br></br>
                 <br></br>
-                <p>Your IP Address: {this.state.result.ip}</p>
+                
             </form>
+            <div>Your IP Address: {content}</div>
+            </div>
         );
     }
 }
