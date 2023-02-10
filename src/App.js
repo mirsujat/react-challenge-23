3d8b9f3c47de676ff404ac484aa20de6904d91a9
Mir Sujat <mir.sujat@gmail.com>
2023-02-10 21:30:15 +0600

    nnn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
+import Challenge from './Components/Challenge/Challenge';
 
 
 const  App = () => {
   return (
     <div className="App">
       Hello world From App
+      <Challenge>
+        <div label="one">
+            <h1>11111111111</h1>
+        </div>
+        <div label="two">
+            <h1>2222222222</h1>
+        </div>
+        <div label="three">
+            <h1>333333333333</h1>
+        </div>
       
+      </Challenge>
     </div>
   );
 }
