32cb269be1aaf74e5f2071f9ab44f45a13b34dc8
Mir Sujat <mir.sujat@gmail.com>
2023-02-25 15:29:09 +0600

    mir

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -1,34 +1,59 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Challenge = () => {
     const [ searchTerm, setSearchTerm ] = useState("");
-    const [result, setResult] = useState({});
-    const [loading, setLoading] = useState(true);
-    const [ errMsg, setErrMsg] = useState("");
+    const [ result, setResult ] = useState({});
+    const [ loading, setLoading ] = useState(true);
+    const [ errMsg, setErrMsg ] = useState("");
+    const [ focus, setFocus ] = useState(false);
+    const inputRef = useRef();
 
-    
-   
+
+
 
     useEffect(() =>{
-        const IPREGEX = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-        const fetchIp = (param) =>{
+        
+        fetchIp(searchTerm || "me");
+
+    }, [ searchTerm])
+
+
+
+
+
+const fetchIp = (param) =>{
+            const IPREGEX = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
             if(IPREGEX.test(param) || param === "me"){
-               return fetch(`https://ip.nf/${param}.json`)
-               .then(res => res.json())
-               .then(res =>  res.ip)
-               .then(data => {
-                    setResult(data)
-                    setLoading(false)
-               }) 
+            return fetch(`https://ip.nf/${param}.json`)
+            .then(res => res.json())
+            .then(res =>  res.ip)
+            .then(data => {
+                setResult(data)
+                setLoading(false)
+                setErrMsg("")
+            }) 
             }else{
-                setErrMsg("Invalid IP Address!!")
-            }
-              
+                setErrMsg("Invalid IP Address!!"); 
+            }   
         }
 
-        fetchIp(searchTerm || "me");
-    }, [ searchTerm ])
 
+
+let errorMsg;
+
+
+const focusHandler = () =>{
+    setFocus(true);
+}
+
+const blurHandler = () =>{
+    setFocus(false);
+}
+
+
+if(errMsg.length > 1 && !focus){
+    errorMsg = <span className="red">{errMsg}</span>
+}
 
     let content;
     if(loading){
@@ -38,20 +63,23 @@ const Challenge = () => {
         content = <span>{result.ip}</span>
     }
 
-    let errorMsg;
-    if(errMsg.length > 1) {
-        errorMsg = <div className='red'>{errMsg}</div>
-    }
+
 
     return(
         <div>
             <h1>Find Your IP Address:</h1>
+
             <input 
                 type="search"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
+                onFocus={focusHandler}
+                onBlur={blurHandler}
+                ref={inputRef}
+            
             />
-            {errorMsg}
+            <div>{ errorMsg }</div>
+            
             <br></br>
             <br></br> 
             <br></br>
