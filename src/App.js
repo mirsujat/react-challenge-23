fb1d8ac6166b036e2efa938fbc6594abb2faff9f
Mir Sujat <mir.sujat@gmail.com>
2023-03-01 12:14:48 +0600

    mir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import Challenge from './Components/Challenge/Challenge';
 
 
 
@@ -8,7 +7,7 @@ const  App = () => {
   return (
     <div className="App">
       Hello world From App
-     <Challenge></Challenge>
+
 
     </div>
   );
