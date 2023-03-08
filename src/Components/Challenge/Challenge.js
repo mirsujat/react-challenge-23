70d2d5a0ddb6a6593883a0086a2071b3eba558eb
Mir Sujat <mir.sujat@gmail.com>
2023-03-08 19:58:34 +0600

    Update Challenge.js

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -4,15 +4,44 @@ import React, { Component } from 'react';
 class Challenge extends Component {
     constructor(props) {
         super(props);
+
         this.state = { activeTab: this.props.children[0].props.label };
+
+        this.navigationKey = {
+            left: 37,
+            up: 38,
+            right: 39,
+            down: 40,
+        }
     }
 
     onClickTabItem = (tab) =>{
-        this.setState({  activeTab : tab })
+        this.selectTab(tab);
     }
 
-    
+    selectTab = (tab) =>{
+         this.setState({  activeTab : tab })
+    }
+    nextTab = (tab) =>{
+        let index = this.props.children.indexOf(tab);
+        if(index < this.props.children.length - 1){
+            let newIndex = index + 1;
+            this.selectTab(this.props.children[newIndex].props.label);
+        }
+        console.log(index);
+    }
 
+    onKeyUp = (e, tab) =>{
+        e.preventDefault();
+        let key = e.keyCode;
+        switch(key){
+            case this.navigationKey.right:
+                this.nextTab(tab)
+                break;
+            default:    
+        }
+
+    }
 
     render() {
         return (
@@ -23,8 +52,9 @@ class Challenge extends Component {
                 <button
                 key={i}
                 role='tab'
-
+                aria-selected={this.state.activeTab === child.props.label}
                 onClick={() => this.onClickTabItem(child.props.label)}
+                onKeyUp={(e) => this.onKeyUp(e, child.props.label)}
 
                 >
                     {child.props.label}
@@ -36,7 +66,7 @@ class Challenge extends Component {
                 {this.props.children.map((child, i) =>{
                     if(this.state.activeTab !== child.props.label) return undefined;
                     return(
-                        <div>
+                        <div key={i}>
                             {child.props.children}
                         </div>
                     )
