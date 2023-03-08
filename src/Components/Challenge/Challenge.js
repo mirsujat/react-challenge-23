a393fef95fd4c54f6d65908332cfec100a133e85
Mir Sujat <mir.sujat@gmail.com>
2023-03-08 22:12:12 +0600

    Update Challenge.js

--- src/Components/Challenge/Challenge.js
+++ src/Components/Challenge/Challenge.js
@@ -5,7 +5,9 @@ class Challenge extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { activeTab: this.props.children[0].props.label };
+        this.tabs = this.props.children;
+        this.focusRef = React.createRef();
+        this.state = { activeTab: false };
 
         this.navigationKey = {
             left: 37,
@@ -13,6 +15,15 @@ class Challenge extends Component {
             right: 39,
             down: 40,
         }
+    }
+
+    componentDidMount() {
+        let tab = this.tabs[0].props.label;
+        this.selectTab(tab);
+    }
+
+    componentDidUpdate(){
+        this.focusRef.focus();
     }
 
     onClickTabItem = (tab) =>{
@@ -23,17 +34,18 @@ class Challenge extends Component {
          this.setState({  activeTab : tab })
     }
     nextTab = (tab) =>{
-        let index = this.props.children.indexOf(tab);
-        if(index < this.props.children.length - 1){
+        let index = this.tabs.indexOf(tab);
+        if(index < this.tabs.length - 1){
             let newIndex = index + 1;
-            this.selectTab(this.props.children[newIndex].props.label);
+            this.selectTab(this.tabs[newIndex].props.label);
         }
         console.log(index);
     }
 
     onKeyUp = (e, tab) =>{
-        e.preventDefault();
+        
         let key = e.keyCode;
+        e.preventDefault();
         switch(key){
             case this.navigationKey.right:
                 this.nextTab(tab)
@@ -48,13 +60,18 @@ class Challenge extends Component {
             <div>
             <h2>React Accessible Tab Component</h2>
             <div className='tablist' role='tablist'>
-            {this.props.children.map((child, i) =>(
+            {this.tabs.map((child, i) =>(
                 <button
                 key={i}
                 role='tab'
+                id={`tab-${i + 1}`}
+                aria-controls={`tabpanel-${i + 1}`}
                 aria-selected={this.state.activeTab === child.props.label}
                 onClick={() => this.onClickTabItem(child.props.label)}
                 onKeyUp={(e) => this.onKeyUp(e, child.props.label)}
+                tabIndex={this.state.activeTab === child.props.label ? 0 : -1}
+                ref={(el) => {if(this.state.activeTab === child.props.label) this.focusRef = el}}
+                className='tab'
 
                 >
                     {child.props.label}
@@ -63,10 +80,18 @@ class Challenge extends Component {
             
             </div>
             <div>
-                {this.props.children.map((child, i) =>{
+                {this.tabs.map((child, i) =>{
                     if(this.state.activeTab !== child.props.label) return undefined;
                     return(
-                        <div key={i}>
+                        <div 
+                        key={i}
+                        tabIndex='0'
+                        role='tabpanel'
+                        id={`tabpanel-${i + 1}`}
+                        aria-labelledby={`tab-${i + 1}`}
+                        aria-hidden={this.state.activeTab === child.props.label ? false : true}
+                        className='tabpanel'
+                        >
                             {child.props.children}
                         </div>
                     )
