dcf4ab7eb4790695eb11781c4826592e49c19f96
Mir Sujat <mir.sujat@gmail.com>
2023-03-11 18:53:11 +0600

    mir

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
-
-
-
-
-
-
-
-
+import Challenge from './Components/Challenge/Challenge';
 
 
 
@@ -15,7 +8,17 @@ const  App = () => {
   return (
     <div className="App">
     <h3>Hello world From App</h3>
-    
+    <Challenge>
+      <div label='one'>
+      <h2>1111111111111111111</h2>
+      </div>
+      <div label='two'>
+      <h2>2222222222222</h2>
+      </div>
+      <div label='three'>
+      <h2>3333333333333</h2>
+      </div>
+    </Challenge>
     </div>
   );
 }
---
+++ src/Components/Challenge/Challenge.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+class Accordion extends Component {
+    static allowMulti = false;
+
+    constructor(props) {
+        super(props);
+
+        const openSection = {};
+
+        this.state = { openSection};
+
+    }
+
+
+
+    onClickOpenSection = (label) =>{
+        const isOpen = !!this.state.openSection[label];
+    
+
+        if(this.props.allowMulti){
+            this.setState({
+                openSection: {
+                    ...this.state.openSection,
+                    [label]: !isOpen
+                }
+            })
+        }else{
+            this.setState({
+                openSection: {
+                    [label] : !isOpen,
+                    expanded: true
+                } 
+            })
+
+        }
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div id="accordionGroup" className="accordion">
+                    {this.props.children.map((child, i) =>(
+                        <div key={i}>
+                            <h3 
+                                open={this.state.openSection[child.props.label]}
+                                onClick={() => this.onClickOpenSection(child.props.label)} 
+                            >
+                            <button
+                            aria-expanded={ this.state.openSection[child.props.label] ? this.state.openSection.expanded : false}
+                            className="accordion-trigger"
+                            aria-controls={`section${i + 1}`}
+                            id={`accordion${i + 1}`}
+                            >
+                            <span className='accordion-title'>
+                                {child.props.label}
+                                <span className="accordion-icon"></span>
+                            
+                            </span>
+                            </button>
+                            </h3>
+                            <div 
+                            id={`section${i + 1}`}
+                            role='region'
+                            aria-labelledby={`accordion${i + 1}`}
+                            className='accordion-panel'
+                            >
+                                {this.state.openSection[child.props.label] && child.props.children}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Accordion;
